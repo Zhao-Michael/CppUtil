5776843633b9385ba540935173444d1efcf72466
MrChenChen <chenchenbbk@hotmail.com>
2016-09-08 17:56:01 +0800

    Add Any Class

--- CppUtil/CppUtil.cpp
+++ CppUtil/CppUtil.cpp
@@ -1,10 +1,36 @@
 #include <Util.h>
+#include <Any.h>
+#include <vector>
+
+vector<Any> mList;
 
 
 int main()
 {
 
-	 
+	int i = 110;
+	string s = "";
+	double d = 1.765;
+	long l = 1290894;
+
+
+	mList.push_back(i);
+
+	mList.push_back(s);
+
+	mList.push_back(d);
+
+	mList.push_back(l);
+
+
+
+
+
+
+	cout << mList[0].cast_type<int>() << endl;
+
+
+
 
 	system("Pause");
 
